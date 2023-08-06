07442138eda88fce5b8255783757ed8173bfda27
aady2test <141581069+aady2test@users.noreply.github.com>
2023-08-07 02:06:54 +0530

    Update streamlit_app.py
    Added pandas lib
    Added lines to pull data from csv file and display it on page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Mom New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Breakfast Menu')
@@ -6,3 +7,5 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑 🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
