b60fda54fd9d34fc969cd8ca7aa6fcfd54b8d6f1
aady2test <141581069+aady2test@users.noreply.github.com>
2023-08-07 02:03:30 +0530

    Update streamlit_app.py
    Fixed error in header
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🥑 🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
